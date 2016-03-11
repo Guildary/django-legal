ea7d37d56b422622160d9de3b92bb7eb73669400
Ryoichiro Kamiya <Ryo@MacBook-Pro.local>
2016-03-11 12:55:08 +0900

    Silence RemovedInDjango110Warning and remove unused south migration file.

--- legal/urls.py
+++ legal/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns, url
-from legal.views import PrivacyPolicyView, TermsOfServiceView, TermsOfServiceAcceptView
+from django.conf.urls import url
+from legal.views import (
+    PrivacyPolicyView, TermsOfServiceView, TermsOfServiceAcceptView
+)
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^privacy-policy/$', PrivacyPolicyView.as_view(), name='privacy_policy'),
     url(r'^terms-of-service/$', TermsOfServiceView.as_view(), name='tos'),
     url(r'terms-of-service-accept/$', TermsOfServiceAcceptView.as_view(), name='tos_accept'),
-)
+]
